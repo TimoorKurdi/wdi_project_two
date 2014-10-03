bc7f0beb5b41800e9ea9953aef909198bf369dc9
TimoorKurdi <timoor.kurdi@gmail.com>
2014-10-02 20:27:56 -0400

    Working on basic functionality

---
+++ project_two/starter_code/public/test.js
@@ -0,0 +1,49 @@
+function createFriend(friend) {
+
+      var friends = friend
+
+        return friends;
+
+    }
+
+  function createFamily(family) {
+
+      var familia = family
+
+        return familia;
+
+    }
+
+  function createCoworkers(work) {
+
+      var job = work
+
+        return job;
+
+    }
+
+  function deleteFriend(frndnomo) {
+
+    var friend = frndnomo;
+
+      return friend;
+
+  }
+
+  function deleteFamily(famnomo) {
+
+    var fam = famnomo;
+
+      return fam;
+
+  }
+
+  function deleteCoworkers(worknomo) {
+
+      var job = worknomo
+
+        return job;
+
+    }
+
+
